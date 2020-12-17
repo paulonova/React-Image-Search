c67a4bb1cb0913db2cb5dd357082bba989c0a2df
Paulo <paulonova144@gmail.com>
2020-12-17 13:45:45 +0100

    Grid layout

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,16 @@
+import './ImageList.css';
 import React from 'react';
+
 
 const ImageList = (props) =>{
 
-  return props.images.map(image => {
-    return <img key={image.id} src={image.urls.regular} alt="saco"/>
+  const images = props.images.map(image => {
+    return <img key={image.id} src={image.urls.regular} alt="saco"/>    
   });
+
+  return (
+    <div className="image-list">{images}</div>
+  );
   
   
 };
