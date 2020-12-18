09bf3836b6b5c238375d4e700e658af74d0f131b
Paulo <paulonova144@gmail.com>
2020-12-18 11:26:50 +0100

    Finishing Grid settings to images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 
 const ImageList = (props) =>{
 
   const images = props.images.map(image => {
-    return <img key={image.id} src={image.urls.regular} alt="saco"/>    
+    return <ImageCard key={image.id} image={image}/>    
   });
 
   return (
